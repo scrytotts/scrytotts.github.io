60b98ebe04b1522def48c84c9e218a156a09da9d
scrytotts <62621372+scrytotts@users.noreply.github.com>
2020-03-24 16:25:24 -0400

    Update scryscraper.js

--- scryscraper.js
+++ scryscraper.js
@@ -1,1 +1,24 @@
 /* Scryfall web scraper script */
+
+
+var getHTML = function ( url, callback ) {
+
+	// Feature detection
+	if ( !window.XMLHttpRequest ) return;
+
+	// Create new request
+	var xhr = new XMLHttpRequest();
+
+	// Setup callback
+	xhr.onload = function() {
+		if ( callback && typeof( callback ) === 'function' ) {
+			callback( this.responseXML );
+		}
+	}
+
+	// Get the HTML
+	xhr.open( 'GET', url );
+	xhr.responseType = 'document';
+	xhr.send();
+
+};
