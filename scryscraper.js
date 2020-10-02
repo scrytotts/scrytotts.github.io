5f142b603ddbc2f12cb184a114352739c6c1bec5
scrytotts <62621372+scrytotts@users.noreply.github.com>
2020-10-02 12:41:13 -0400

    Update scryscraper.js

--- scryscraper.js
+++ scryscraper.js
@@ -3,6 +3,6 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-if (urlParams.has('json-data')) {
+if ( urlParams.has('json-data') ) {
     console.log('success');
 }
