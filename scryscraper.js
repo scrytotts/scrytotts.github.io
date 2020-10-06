695928122d69fc43063d0edfeb081ea567317867
scrytotts <62621372+scrytotts@users.noreply.github.com>
2020-10-06 13:10:20 -0400

    Update scryscraper.js

--- scryscraper.js
+++ scryscraper.js
@@ -1,10 +1,4 @@
 /* json morpher script */
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
-if ( urlParams.has('json-data') ) {
-    var data = urlParams.get('json-data');
-    var obj = JSON.parse(data);
-    console.log(obj);
-}
+var data = document.getElementById('data').innerText;
+var obj = JSON.parse(data);
