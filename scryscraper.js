6a15a57d03235b9a76864c23751691781e7623da
scrytotts <62621372+scrytotts@users.noreply.github.com>
2020-12-07 16:30:41 -0500

    Update scryscraper.js

--- scryscraper.js
+++ scryscraper.js
@@ -1,5 +1,12 @@
 /* json morpher script */
-function convert() {
+function convert() { 
   var textbox = document.getElementById('data');
-  textbox.innerHTML = "TEST"
+  
+  var input = textbox.innerText;
+  
+  var inputJSON = JSON.parse(input);
+  
+  console.log(inputJSON);
+  
+  //textbox.innerHTML = "TEST"
 }
